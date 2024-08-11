9972c8b0f64c8d09f740d0c0905a74859d4c7dc4
Hoang_2506 <dohuyhoang25062000@gmail.com>
2024-08-11 23:15:05 +0700

    Create Server and Connect Database

---
+++ config/dbConfig.js
@@ -0,0 +1,26 @@
+const sql = require('mssql');
+
+const config = {
+    user: 'SQLAdmin',
+    password: '12345678@a',
+    server: 'mockprojecsqlserver.database.windows.net',
+    port: 1433, // Sử dụng port mặc định của SQL Server
+    database: 'newbodyguardDB',
+    options: {
+        encrypt: true // Bắt buộc đối với Azure SQL
+    }
+};
+
+async function connectAndQuery() {
+    try {
+        // Tạo kết nối đến cơ sở dữ liệu
+        const pool = await sql.connect(config);
+        console.log('Connected to SQL Server');
+    } catch (err) {
+        console.error('Database Connection Failed! Bad Config: ', err);
+    }
+}
+
+// Gọi hàm để kiểm tra kết nối và truy vấn
+console.log("Starting...");
+connectAndQuery();
